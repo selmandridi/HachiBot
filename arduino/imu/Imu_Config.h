5ff3c84ba7f75fdf307775c451c2eea4d1c36736
Selman Dridi <dridi.selman@gmail.com>
2021-02-10 15:29:41 +0100

    Add imu driver

---
+++ arduino/imu/Imu_Config.h
@@ -0,0 +1,20 @@
+#ifndef _IMU_CONFIG_H_
+#define _IMU_CONFIG_H_
+
+#include "I2Cdev.h"
+#include "MPU9250.h"
+
+#define G_TO_ACCEL 9.81
+#define MGAUSS_TO_UTESLA 0.1
+#define UTESLA_TO_TESLA 0.000001
+#define ACCEL_SCALE 1 / 16384 // LSB/g
+#define GYRO_SCALE 1 / 131 // LSB/(deg/s)
+#define MAG_SCALE 0.6 // uT/LSB
+    
+MPU9250 accelerometer;
+MPU9250 gyroscope;    
+MPU9250 magnetometer;
+
+#endif
+
+#endif
